c925526165db1fcc645c5344b7ea8f2313d00a57
Venkat Ranganathan <96662693+venkat-ranganathan@users.noreply.github.com>
2022-02-07 14:06:08 -0500

    Add files via upload

---
+++ Data_Structures_Algorithms/Labs/Lab_1/lab1.c
@@ -0,0 +1,29 @@
+/******************************* SAMPLE TEST FILE ****************************/
+/******************** NO IMPLEMENTATION HERE, ONLY TESTS *********************/
+// two includes here to test for include guards in the header file 
+//#include "lab1dev.h"
+#include "lab1dev.h"
+#include "lab1dev.h"
+
+int main(void)
+{
+    char name[] = "CS212";
+
+    welcome(name);
+
+    printf("########## Test-1: leftmostOnePos(4) ##########\n");
+    assert(leftmostOnePos(4) == 3);
+    printf("> Passed Test-1!\n");
+
+    printf("########## Test-2: leftmostOnePos(250) ##########\n");
+    assert(leftmostOnePos(250) == 8);
+    printf("> Passed Test-2!\n");
+
+    printf("########## Test-3: leftmostOnePos(SHRT_MAX) ##########\n");
+    assert(leftmostOnePos(SHRT_MAX) == 15);
+    printf("> Passed Test-3!\n");
+
+    printf("> Passed leftmostOnePos() tests!\n\n");
+
+    printf("> All Tests Passed Successfully!\n");
+}
---
+++ Data_Structures_Algorithms/Labs/Lab_1/lab1dev.c
@@ -0,0 +1,32 @@
+#include "lab1dev.h"
+#ifndef LAB1DEV_C
+#define LAB1DEV_C
+
+// This program should 1) Print a welcome message & 2) find the leftmost
+// one in the binary representation of a given number
+
+void welcome(char name[])
+{
+    printf("Hello, %s\n", name);
+}
+
+unsigned int leftmostOnePos(unsigned int number)
+{
+	if (number == 0)
+		return 0;
+
+	int msb = 1;
+	number = number / 2;
+	while (number != 0) 
+    {
+		number = number / 2;
+		msb++;
+	}
+
+    //printf("left most one pos is: %i\n",msb+1);
+
+	return msb;
+  
+}
+
+#endif
