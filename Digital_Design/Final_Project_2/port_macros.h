b897cf28a1bebeea44a8a2842ffc418c1b094ae3
venkat-ranganathan <prasanna.rangananthan@icloud.com>
2022-04-22 15:56:48 -0400

    Triceratops

---
+++ Digital_Design/Final_Project_2/port_macros.h
@@ -0,0 +1,16 @@
+#ifndef PORT_MACROS_H
+#define PORT_MACROS_H
+
+#define CONCATENATE(A, B) A##B
+#define CONCATENATE3(A, B, C) A##B##C
+
+#define PORT(letter) CONCATENATE(PORT, letter)
+#define DDR(letter) CONCATENATE(DDR, letter)
+#define PIN(letter) CONCATENATE(PIN, letter)
+#define PULLUP(letter) CONCATENATE(PORT, letter)
+#define TOGGLE(letter) CONCATENATE(PIN, letter)
+
+#define TCCR(number, letter) CONCATENATE3(TCCR, number, letter)
+#define OCR(number, letter) CONCATENATE3(OCR, number, letter)
+
+#endif
