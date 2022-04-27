fda0fdc64fb7dd4680bd1cee1f6bccda184decef
venkat-ranganathan <prangan1@binghamton.edu>
2022-04-27 14:25:34 -0400

    Stegosaurus

--- Digital_Design/Final_Project_2/main.c
+++ Digital_Design/Final_Project_2/main.c
@@ -8,17 +8,11 @@
 #include <avr/io.h>
 #include "lcd_driver.h"
 #include "port_macros.h"
-#define PWM_TOP 500
+//#define PWM_TOP 500
 
 int main()
 {
-	int RS_status_0 = 0;
-	int RS_status_1 = 0;
-	int RS_status_2 = 0;
-	int RS_status_3 = 0;
-	int RS_status_4 = 0;
-
-	int time = 39999;
+	int RS_status[] = {0, 0, 0, 0, 0};
 
 	// Setting reflectance sensors as inputs
 
@@ -27,137 +21,64 @@ int main()
 	DDRC &= ~(1 << 2);
 	DDRC &= ~(1 << 3);
 	DDRC &= ~(1 << 4);
-	PORTC |= (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
+
+	// Set register values as '1' to turn on sensors
+
+	PORTC |= (1 << 0);
+	PORTC |= (1 << 1);
+	PORTC |= (1 << 2);
+	PORTC |= (1 << 3);
+	PORTC |= (1 << 4);
 
 	initialize_LCD_driver();
 	LCD_execute_command(TURN_ON_DISPLAY);
 
 	while (1)
 	{
-		while (time < 40000)
+		// Loops through sensors 0 to 4 to check status
+
+		for (int i = 0; i < 5; i++)
 		{
-			;
-			// Live printing reflectance sensor status to LCD
-			if ((PINC & (1 << 0)) == 1)
+			if ((PINC & (1 << i)) >= 1)
 			{
-				// Print out status of PC0 reflectance sensor
-				RS_status_0 = 1;
-				char char_RS_status_0[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(0, 0);
-				sprintf(char_RS_status_0, "%d", RS_status_0);
-				LCD_print_String(char_RS_status_0); // Prints xcoord
-				_delay_us(10);
+				RS_status[i] = 1;
 			}
 			else
 			{
-				// Print out status of PC0 reflectance sensor
-				RS_status_0 = 0;
-				char char_RS_status_0[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(0, 0);
-				sprintf(char_RS_status_0, "%d", RS_status_0);
-				LCD_print_String(char_RS_status_0); // Prints xcoord
-				_delay_us(10);
-			}
-			if ((PINC & (1 << 1)) == 1)
-			{
-				// Print out status of PC1 reflectance sensor
-				RS_status_1 = 1;
-				char char_RS_status_1[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(1, 0);
-				sprintf(char_RS_status_1, "%d", RS_status_1);
-				LCD_print_String(char_RS_status_1); // Prints xcoord
-				_delay_us(10);
-			}
-			else
-			{
-				// Print out status of PC1 reflectance sensor
-				RS_status_1 = 0;
-				char char_RS_status_1[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(1, 0);
-				sprintf(char_RS_status_1, "%d", RS_status_1);
-				LCD_print_String(char_RS_status_1); // Prints xcoord
-				_delay_us(10);
-			}
-			if ((PINC & (1 << 2)) == 1)
-			{
-				// Print out status of PC2 reflectance sensor
-				RS_status_2 = 1;
-				char char_RS_status_2[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(2, 0);
-				sprintf(char_RS_status_2, "%d", RS_status_2);
-				LCD_print_String(char_RS_status_2); // Prints xcoord
-				_delay_us(10);
-			}
-			else
-			{
-				// Print out status of PC2 reflectance sensor
-				RS_status_2 = 0;
-				char char_RS_status_2[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(2, 0);
-				sprintf(char_RS_status_2, "%d", RS_status_2);
-				LCD_print_String(char_RS_status_2); // Prints xcoord
-				_delay_us(10);
-			}
-			if ((PINC & (1 << 3)) == 1)
-			{
-				// Print out status of PC3 reflectance sensor
-				RS_status_3 = 1;
-				char char_RS_status_3[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(3, 0);
-				sprintf(char_RS_status_3, "%d", RS_status_3);
-				LCD_print_String(char_RS_status_3); // Prints xcoord
-				_delay_us(10);
-			}
-			else
-			{
-				// Print out status of PC3 reflectance sensor
-				RS_status_3 = 0;
-				char char_RS_status_3[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(3, 0);
-				sprintf(char_RS_status_3, "%d", RS_status_3);
-				LCD_print_String(char_RS_status_3); // Prints xcoord
-				_delay_us(10);
-			}
-			if ((PINC & (1 << 4)) == 1)
-			{
-				// Print out status of PC4 reflectance sensor
-				RS_status_4 = 1;
-				char char_RS_status_4[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(4, 0);
-				sprintf(char_RS_status_4, "%d", RS_status_4);
-				LCD_print_String(char_RS_status_4); // Prints xcoord
-				_delay_us(10);
-			}
-			else
-			{
-				// Print out status of PC4 reflectance sensor
-				RS_status_4 = 0;
-				char char_RS_status_4[8];
-
-				// LCD_execute_command(CLEAR_DISPLAY);
-				LCD_move_cursor_to_col_row(4, 0);
-				sprintf(char_RS_status_4, "%d", RS_status_4);
-				LCD_print_String(char_RS_status_4); // Prints xcoord
-				_delay_us(10);
+				RS_status[i] = 0;
 			}
 		}
+		char char_RS_status_0[8];
+		char char_RS_status_1[8];
+		char char_RS_status_2[8];
+		char char_RS_status_3[8];
+		char char_RS_status_4[8];
+
+		LCD_execute_command(CLEAR_DISPLAY);
+		LCD_move_cursor_to_col_row(0, 0);
+		sprintf(char_RS_status_0, "%d", RS_status[0]);
+		LCD_print_String(char_RS_status_0);
+		//_delay_ms(50);
+
+		// LCD_execute_command(CLEAR_DISPLAY);
+		LCD_move_cursor_to_col_row(1, 0);
+		sprintf(char_RS_status_1, "%d", RS_status[1]);
+		LCD_print_String(char_RS_status_1);
+		//_delay_ms(50);
+
+		LCD_move_cursor_to_col_row(2, 0);
+		sprintf(char_RS_status_2, "%d", RS_status[2]);
+		LCD_print_String(char_RS_status_2);
+		//_delay_ms(50);
+
+		LCD_move_cursor_to_col_row(3, 0);
+		sprintf(char_RS_status_3, "%d", RS_status[3]);
+		LCD_print_String(char_RS_status_3);
+		//_delay_ms(50);
+
+		LCD_move_cursor_to_col_row(4, 0);
+		sprintf(char_RS_status_4, "%d", RS_status[4]);
+		LCD_print_String(char_RS_status_4);
+		_delay_ms(50);
 	}
 }
