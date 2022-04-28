b0635943a7d9a6e525e5ae2029f8537d7a663cf0
venkat-ranganathan <prangan1@binghamton.edu>
2022-04-28 13:10:25 -0400

    Pterosaurus

--- Digital_Design/Final_Project_2/main.c
+++ Digital_Design/Final_Project_2/main.c
@@ -8,10 +8,24 @@
 #include <avr/io.h>
 #include "lcd_driver.h"
 #include "port_macros.h"
-//#define PWM_TOP 500
+#define PWM_TOP 500
+
+void rotate_90_left(long rot_time_L, unsigned int duty_cycleL, unsigned int duty_cycleR);
+void rotate_90_right(long rot_time_R, unsigned int duty_cycleL, unsigned int duty_cycleR);
+void about_face(long aboutface_time, unsigned int duty_cycleL, unsigned int duty_cycleR);
 
 int main()
 {
+	// Left and Right motor settings
+
+	long rot_time_L = 12750;
+	long rot_time_R = 13980;
+	long aboutface_time = 21000;
+	unsigned int duty_cycleL = 100;
+	unsigned int duty_cycleR = 103;
+
+	// Reflectance sensor variables set as an array
+
 	int RS_status[] = {0, 0, 0, 0, 0};
 
 	// Setting reflectance sensors as inputs
@@ -22,13 +36,25 @@ int main()
 	DDRC &= ~(1 << 3);
 	DDRC &= ~(1 << 4);
 
-	// Set register values as '1' to turn on sensors
+	// Set reflectance sensor register values as '1' to turn on sensors
 
 	PORTC |= (1 << 0);
 	PORTC |= (1 << 1);
 	PORTC |= (1 << 2);
 	PORTC |= (1 << 3);
 	PORTC |= (1 << 4);
+
+	// Left and Right motors set as outputs and pull-up resistors enabled
+
+	DDRD |= (1 << 5) | (1 << 6); // Left Motor
+	DDRD |= (1 << 3);			 // Right Motor
+	DDRB |= (1 << 3);			 // Right Motor
+	DDRB &= ~(1 << 1);			 // Configure left push-button input
+	PORTB |= (1 << 1);			 // enable pull-up resistor
+	DDRB &= ~(1 << 4);			 // Configure middle push-button input
+	PORTB |= (1 << 4);			 // enable pull-up resistor
+	DDRB &= ~(1 << 5);			 // Configure right push-button input
+	PORTB |= (1 << 5);			 // enable pull-up resistor
 
 	initialize_LCD_driver();
 	LCD_execute_command(TURN_ON_DISPLAY);
@@ -81,4 +107,172 @@ int main()
 		LCD_print_String(char_RS_status_4);
 		_delay_ms(50);
 	}
+}
+//=============MOVE_Y================
+void move_y(long distance_time_y, unsigned int duty_cycleL, unsigned int duty_cycleR)
+{
+	long time = 0;
+	unsigned int pwm_counter = 0;
+	_delay_ms(1000);
+	while (1)
+	{
+		pwm_counter++;
+		if (pwm_counter >= PWM_TOP)
+		{
+			pwm_counter = 0;
+		}
+		if (pwm_counter < duty_cycleL)
+		{
+			PORTD &= ~(1 << 5);
+			PORTD |= (1 << 6);
+		} // LEFT FORWARD
+		else
+		{
+			PORTD &= ~((1 << 5) | (1 << 6));
+		} // COAST MOTORS
+		if (pwm_counter < duty_cycleR)
+		{
+			PORTD &= ~(1 << 3);
+			PORTB |= (1 << 3);
+		} // RIGHT FORWARD
+		else
+		{
+			PORTD &= ~((1 << 3)); // COAST MOTORS
+			PORTB &= ~(1 << 3);
+		}
+		if (time == distance_time_y)
+		{
+			break;
+		}
+		time++;
+		_delay_us(10);
+	}
+	PORTD |= (1 << 3) | (1 << 5) | (1 << 6); // BRAKE MOTORS
+	PORTB |= (1 << 3);
+}
+//=============ROTATE_90_LEFT================
+void rotate_90_left(long rot_time_L, unsigned int duty_cycleL, unsigned int duty_cycleR)
+{
+	long time = 0;
+	unsigned int pwm_counter = 0;
+	_delay_ms(1000);
+	while (1)
+	{
+		pwm_counter++;
+		if (pwm_counter >= PWM_TOP)
+		{
+			pwm_counter = 0;
+		}
+		if (pwm_counter < duty_cycleL)
+		{
+			PORTD |= (1 << 5);
+			PORTD &= ~(1 << 6);
+		} // LEFT REVERSE
+		else
+		{
+			PORTD &= ~((1 << 5) | (1 << 6));
+		} // COAST MOTORS
+		if (pwm_counter < duty_cycleR)
+		{
+			PORTD &= ~(1 << 3);
+			PORTB |= (1 << 3);
+		} // RIGHT FORWARD
+		else
+		{
+			PORTD &= ~((1 << 3)); // COAST MOTORS
+			PORTB &= ~(1 << 3);
+		}
+		if (time == rot_time_L)
+		{
+			break;
+		}
+		time++;
+		_delay_us(10);
+	}
+	PORTD |= (1 << 3) | (1 << 5) | (1 << 6); // BRAKE MOTORS
+	PORTB |= (1 << 3);
+}
+//=============ROTATE_90_RIGHT================
+void rotate_90_right(long rot_time_R, unsigned int duty_cycleL, unsigned int duty_cycleR)
+{
+	long time = 0;
+	unsigned int pwm_counter = 0;
+	_delay_ms(1000);
+	while (1)
+	{
+		pwm_counter++;
+		if (pwm_counter >= PWM_TOP)
+		{
+			pwm_counter = 0;
+		}
+		if (pwm_counter < duty_cycleL)
+		{
+			PORTD &= ~(1 << 5);
+			PORTD |= (1 << 6);
+		} // LEFT FORWARD
+		else
+		{
+			PORTD &= ~((1 << 5) | (1 << 6));
+		} // COAST MOTORS
+		if (pwm_counter < duty_cycleR)
+		{
+			PORTD |= (1 << 3);
+			PORTB &= ~(1 << 3);
+		} // RIGHT REVERSE
+		else
+		{
+			PORTD &= ~((1 << 3)); // COAST MOTORS
+			PORTB &= ~(1 << 3);
+		}
+		if (time == rot_time_R)
+		{
+			break;
+		}
+		time++;
+		_delay_us(10);
+	}
+	PORTD |= (1 << 3) | (1 << 5) | (1 << 6); // BRAKE MOTORS
+	PORTB |= (1 << 3);
+}
+//===============ABOUT_FACE===(to the left)=============
+void about_face(long aboutface_time, unsigned int duty_cycleL, unsigned int duty_cycleR)
+{
+	long time = 0;
+	unsigned int pwm_counter = 0;
+	_delay_ms(1000);
+	while (1)
+	{
+		pwm_counter++;
+		if (pwm_counter >= PWM_TOP)
+		{
+			pwm_counter = 0;
+		}
+		if (pwm_counter < duty_cycleL)
+		{
+			PORTD |= (1 << 5);
+			PORTD &= ~(1 << 6);
+		} // LEFT REVERSE
+		else
+		{
+			PORTD &= ~((1 << 5) | (1 << 6));
+		} // COAST MOTORS
+		if (pwm_counter < duty_cycleR)
+		{
+			PORTD &= ~(1 << 3);
+			PORTB |= (1 << 3);
+		} // RIGHT FORWARD
+		else
+		{
+			PORTD &= ~((1 << 3)); // COAST MOTORS
+			PORTB &= ~(1 << 3);
+		}
+		if (time == aboutface_time)
+		{
+			break;
+		}
+		_delay_us(10);
+		time++;
+	}
+	PORTD |= (1 << 3) | (1 << 5) | (1 << 6); // BRAKE MOTORS
+	PORTB |= (1 << 3);
 }
