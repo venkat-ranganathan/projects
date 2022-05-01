b767e5f1f8142610db668e5b1f3a478b880487c8
venkat-ranganathan <prangan1@binghamton.edu>
2022-05-01 11:02:18 -0400

    Utahraptor

--- Digital_Design/Final_Project_2/main.c
+++ Digital_Design/Final_Project_2/main.c
@@ -1,4 +1,4 @@
-// Ben Telfer and Prasanna Ranganathan
+// Prasanna Ranganathan and Ben Telfer
 // EECE 287 - Spring 2022 - Binghamton University
 
 #include <stdint.h>
@@ -10,11 +10,11 @@
 #include "port_macros.h"
 #define PWM_TOP 500
 
-// Need LCD function?
-void move_y(long distance_time_y, unsigned int duty_cycleL, unsigned int duty_cycleR);
+void display(int RS_0, int RS_1, int RS_2, int RS_3, int RS_4);
+void navigate(int RS_0, int RS_1, int RS_2, int RS_3, int RS_4);
+void move(long distance_time, unsigned int duty_cycleL, unsigned int duty_cycleR);
 void rotate_90_left(long rot_time_L, unsigned int duty_cycleL, unsigned int duty_cycleR);
 void rotate_90_right(long rot_time_R, unsigned int duty_cycleL, unsigned int duty_cycleR);
-void about_face(long aboutface_time, unsigned int duty_cycleL, unsigned int duty_cycleR);
 
 int main()
 {
@@ -22,13 +22,13 @@ int main()
 
 	long rot_time_L = 12750;
 	long rot_time_R = 13980;
-	long aboutface_time = 21000;
+
 	unsigned int duty_cycleL = 100;
 	unsigned int duty_cycleR = 103;
 
-	// Reflectance sensor variables set as an array
-
-	int RS_status[] = {0, 0, 0, 0, 0};
+	long ycoord = 0;
+	long absycoord = 0;
+	long distance_time = 0;
 
 	// Setting reflectance sensors as inputs
 
@@ -38,7 +38,7 @@ int main()
 	DDRC &= ~(1 << 3);
 	DDRC &= ~(1 << 4);
 
-	// Set reflectance sensor register values as '1' to turn on sensors
+	// Set reflectance sensor register values as '1' to turn on sensors by setting it to 5V
 
 	PORTC |= (1 << 0);
 	PORTC |= (1 << 1);
@@ -63,10 +63,17 @@ int main()
 
 	while (1)
 	{
+		// Reflectance sensor variables set as an array
+
+		int RS_status[] = {0, 0, 0, 0, 0};
+
 		// Loops through sensors 0 to 4 to check status
 
 		for (int i = 0; i < 5; i++)
 		{
+			// PIN# provides read-only data state from the corresponding PORT#
+			// Conditional checks if read-only PIN register is high, and sets RS_status to 1, else 0
+
 			if ((PINC & (1 << i)) >= 1)
 			{
 				RS_status[i] = 1;
@@ -76,46 +83,293 @@ int main()
 				RS_status[i] = 0;
 			}
 		}
-		char char_RS_status_0[8]; // Maybe try char char_RS_status_[i][8]?
-		char char_RS_status_1[8];
-		char char_RS_status_2[8];
-		char char_RS_status_3[8];
-		char char_RS_status_4[8];
 
-		LCD_execute_command(CLEAR_DISPLAY);
-		LCD_move_cursor_to_col_row(0, 0);
-		sprintf(char_RS_status_0, "%d", RS_status[0]);
-		LCD_print_String(char_RS_status_0);
-		//_delay_ms(50);
+		// Calls navigation function
 
-		// LCD_execute_command(CLEAR_DISPLAY);
-		LCD_move_cursor_to_col_row(1, 0);
-		sprintf(char_RS_status_1, "%d", RS_status[1]);
-		LCD_print_String(char_RS_status_1);
-		//_delay_ms(50);
+		navigate(RS_status[0], RS_status[1], RS_status[2], RS_status[3], RS_status[4]);
 
-		LCD_move_cursor_to_col_row(2, 0);
-		sprintf(char_RS_status_2, "%d", RS_status[2]);
-		LCD_print_String(char_RS_status_2);
-		//_delay_ms(50);
+		// Calls display function
 
-		LCD_move_cursor_to_col_row(3, 0);
-		sprintf(char_RS_status_3, "%d", RS_status[3]);
-		LCD_print_String(char_RS_status_3);
-		//_delay_ms(50);
-
-		LCD_move_cursor_to_col_row(4, 0);
-		sprintf(char_RS_status_4, "%d", RS_status[4]);
-		LCD_print_String(char_RS_status_4);
-		_delay_ms(50);
+		display(RS_status[0], RS_status[1], RS_status[2], RS_status[3], RS_status[4]);
 	}
 }
-//=============MOVE_Y================
-void move_y(long distance_time_y, unsigned int duty_cycleL, unsigned int duty_cycleR)
+
+// Display function shows status of reflectance sensors on LCD display
+
+void display(int RS_0, int RS_1, int RS_2, int RS_3, int RS_4)
+{
+
+	// Create RS_status character variables to output to LCD
+
+	char char_RS_status_0[8];
+	char char_RS_status_1[8];
+	char char_RS_status_2[8];
+	char char_RS_status_3[8];
+	char char_RS_status_4[8];
+
+	// Clear display once at start
+
+	LCD_execute_command(CLEAR_DISPLAY);
+
+	// Print out reflectance sensor states to LCD
+
+	LCD_move_cursor_to_col_row(0, 0);
+	sprintf(char_RS_status_0, "%d", RS_0);
+	LCD_print_String(char_RS_status_0);
+
+	LCD_move_cursor_to_col_row(1, 0);
+	sprintf(char_RS_status_1, "%d", RS_1);
+	LCD_print_String(char_RS_status_1);
+
+	LCD_move_cursor_to_col_row(2, 0);
+	sprintf(char_RS_status_2, "%d", RS_2);
+	LCD_print_String(char_RS_status_2);
+
+	LCD_move_cursor_to_col_row(3, 0);
+	sprintf(char_RS_status_3, "%d", RS_3);
+	LCD_print_String(char_RS_status_3);
+
+	LCD_move_cursor_to_col_row(4, 0);
+	sprintf(char_RS_status_4, "%d", RS_4);
+	LCD_print_String(char_RS_status_4);
+
+	// Add small delay at end
+
+	_delay_ms(50);
+}
+
+// Navigation function
+
+void navigate(int RS_0, int RS_1, int RS_2, int RS_3, int RS_4)
+{
+	long rot_time_L = 1000;
+	long rot_time_R = 1600;
+
+	unsigned int duty_cycleL = 100;
+	unsigned int duty_cycleR = 150;
+
+	long distance_time = 450;
+
+	// Conditionals to navigate forward
+
+	if ((((RS_1 & RS_3) >= 1)))
+	{
+		// Move straight
+
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	if ((((RS_1 & RS_2 & RS_3) >= 1)))
+	{
+		// Move straight
+
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct right oversteer
+
+	if ((((RS_1 & RS_2) >= 1)))
+	{
+		//_delay_ms(1);
+		// Move straight
+		duty_cycleL = 0;
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct significant right oversteer
+
+	if ((((RS_2 & RS_3) >= 1)))
+	{
+		// Move straight
+		duty_cycleR = 0;
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct left oversteer
+
+	if ((((RS_3 & RS_4) >= 1)))
+	{
+		//_delay_ms(1);
+		// Move straight
+		duty_cycleR = 0;
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct significant left oversteer
+
+	if ((((RS_0 & RS_1 & RS_2) >= 1)))
+	{
+		// Move straight
+		// duty_cycleL = 0;
+		rot_time_L = 3000;
+		rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct significant left oversteer
+
+	if ((((RS_0 & RS_1 & RS_2 & RS_3) >= 1)))
+	{
+		// Move straight
+		// duty_cycleL = 0;
+		rot_time_L = 3000;
+		rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	if ((((RS_0 & RS_1 & RS_2 & RS_4) >= 1)))
+	{
+		// Move straight
+		// duty_cycleL = 0;
+		rot_time_L = 3000;
+		rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct significant left oversteer
+
+	if ((((RS_1 & RS_2 & RS_3 & RS_4) >= 1)))
+	{
+		// Move straight
+		// duty_cycleR = 0;
+		rot_time_R = 4000;
+		rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	if ((((RS_0 & RS_2 & RS_3 & RS_4) >= 1)))
+	{
+		// Move straight
+		// duty_cycleR = 0;
+		rot_time_R = 4000;
+		rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct significant left oversteer
+
+	if ((((RS_2 & RS_3 & RS_4) >= 1)))
+	{
+		// Move straight
+		// duty_cycleR = 10;
+		rot_time_R = 4000;
+		rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct edge of grid
+
+	if ((((RS_0 & RS_4) >= 1)))
+	{
+		int r = rand() % 2;
+		// Move straight
+
+		if (r = 0)
+		{
+			rot_time_R = 2000;
+			rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+			move(distance_time, duty_cycleL, duty_cycleR);
+		}
+
+		if (r = 1)
+		{
+			rot_time_L = 2000;
+			rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+			move(distance_time, duty_cycleL, duty_cycleR);
+		}
+	}
+
+	// Conditional to correct edge of grid
+
+	if ((((RS_0) >= 1) & (RS_1 & RS_2 & RS_3 & RS_4) <= 0))
+	{
+		// Move straight
+		rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct edge of grid
+
+	if ((((RS_4) >= 1) & (RS_0 & RS_1 & RS_2 & RS_3) <= 0))
+	{
+		// Move straight
+		rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct edge of grid
+
+	if ((((RS_3) >= 1) & (RS_0 & RS_1 & RS_2 & RS_4) <= 0))
+	{
+		// Move straight
+		rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct edge of grid
+
+	if ((((RS_2) >= 1) & (RS_0 & RS_1 & RS_3 & RS_4) <= 0))
+	{
+		// Move straight
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct edge of grid
+
+	if ((((RS_1) >= 1) & (RS_0 & RS_2 & RS_3 & RS_4) <= 0))
+	{
+		// Move straight
+		rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+		move(distance_time, duty_cycleL, duty_cycleR);
+	}
+
+	// Conditional to correct edge of grid
+
+	if ((RS_0 & RS_1 & RS_2 & RS_3 & RS_4) <= 0)
+	{
+		int r = rand() % 2;
+		// Move straight
+
+		if (r = 0)
+		{
+			rot_time_R = 2000;
+			rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+			move(distance_time, duty_cycleL, duty_cycleR);
+		}
+
+		if (r = 1)
+		{
+			rot_time_L = 2000;
+			rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+			move(distance_time, duty_cycleL, duty_cycleR);
+		}
+	}
+
+	if ((RS_0 & RS_1 & RS_2 & RS_3 & RS_4) >= 1)
+	{
+		int r = rand() % 2;
+		// Move straight
+
+		if (r = 0)
+		{
+			rot_time_R = 2000;
+			rotate_90_right(rot_time_R, duty_cycleL, duty_cycleR);
+			move(distance_time, duty_cycleL, duty_cycleR);
+		}
+
+		if (r = 1)
+		{
+			rot_time_L = 2000;
+			rotate_90_left(rot_time_L, duty_cycleL, duty_cycleR);
+			move(distance_time, duty_cycleL, duty_cycleR);
+		}
+	}
+}
+//=============move================
+void move(long distance_time, unsigned int duty_cycleL, unsigned int duty_cycleR)
 {
 	long time = 0;
 	unsigned int pwm_counter = 0;
-	_delay_ms(1000);
 	while (1)
 	{
 		pwm_counter++;
@@ -142,7 +396,7 @@ void move_y(long distance_time_y, unsigned int duty_cycleL, unsigned int duty_cy
 			PORTD &= ~((1 << 3)); // COAST MOTORS
 			PORTB &= ~(1 << 3);
 		}
-		if (time == distance_time_y)
+		if (time == distance_time)
 		{
 			break;
 		}
@@ -151,13 +405,19 @@ void move_y(long distance_time_y, unsigned int duty_cycleL, unsigned int duty_cy
 	}
 	PORTD |= (1 << 3) | (1 << 5) | (1 << 6); // BRAKE MOTORS
 	PORTB |= (1 << 3);
+
+	// Coast left and right motors
+
+	// PORTD &= ~((1 << 5) | (1 << 6));
+	// PORTD &= ~((1 << 3));
+	// PORTB &= ~(1 << 3);
 }
 //=============ROTATE_90_LEFT================
 void rotate_90_left(long rot_time_L, unsigned int duty_cycleL, unsigned int duty_cycleR)
 {
 	long time = 0;
 	unsigned int pwm_counter = 0;
-	_delay_ms(1000);
+	//_delay_ms(1000);
 	while (1)
 	{
 		pwm_counter++;
@@ -199,7 +459,7 @@ void rotate_90_right(long rot_time_R, unsigned int duty_cycleL, unsigned int dut
 {
 	long time = 0;
 	unsigned int pwm_counter = 0;
-	_delay_ms(1000);
+	//_delay_ms(1000);
 	while (1)
 	{
 		pwm_counter++;
@@ -232,48 +492,6 @@ void rotate_90_right(long rot_time_R, unsigned int duty_cycleL, unsigned int dut
 		}
 		time++;
 		_delay_us(10);
-	}
-	PORTD |= (1 << 3) | (1 << 5) | (1 << 6); // BRAKE MOTORS
-	PORTB |= (1 << 3);
-}
-//===============ABOUT_FACE===(to the left)=============
-void about_face(long aboutface_time, unsigned int duty_cycleL, unsigned int duty_cycleR)
-{
-	long time = 0;
-	unsigned int pwm_counter = 0;
-	_delay_ms(1000);
-	while (1)
-	{
-		pwm_counter++;
-		if (pwm_counter >= PWM_TOP)
-		{
-			pwm_counter = 0;
-		}
-		if (pwm_counter < duty_cycleL)
-		{
-			PORTD |= (1 << 5);
-			PORTD &= ~(1 << 6);
-		} // LEFT REVERSE
-		else
-		{
-			PORTD &= ~((1 << 5) | (1 << 6));
-		} // COAST MOTORS
-		if (pwm_counter < duty_cycleR)
-		{
-			PORTD &= ~(1 << 3);
-			PORTB |= (1 << 3);
-		} // RIGHT FORWARD
-		else
-		{
-			PORTD &= ~((1 << 3)); // COAST MOTORS
-			PORTB &= ~(1 << 3);
-		}
-		if (time == aboutface_time)
-		{
-			break;
-		}
-		_delay_us(10);
-		time++;
 	}
 	PORTD |= (1 << 3) | (1 << 5) | (1 << 6); // BRAKE MOTORS
 	PORTB |= (1 << 3);
