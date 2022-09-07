0f75b031804a6acca700edc1210b242abf6759e7
venkat-ranganathan <prangan1@binghamton.edu>
2022-09-07 18:22:25 -0400

    graciloraptor

---
+++ C_Programming/Lab2/lab2.h
@@ -0,0 +1,17 @@
+#ifndef LAB2_H
+#define LAB2_H
+
+#include <stdio.h>
+#include <assert.h>
+
+#define PI 3.14
+
+void introduction(void);
+float castToFloat(int numi);
+double circleArea(int radius);
+int circlePerimeter(int radius);
+int fahrenheit2Celsius(int fahrenheit);
+unsigned int factorial(unsigned int num);
+
+#endif
+
