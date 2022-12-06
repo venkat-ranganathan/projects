0bee838525e797d8757e212ce80617ba1f7029c2
venkat-ranganathan <prangan1@binghamton.edu>
2022-12-05 21:50:31 -0500

    Alioramus

---
+++ C_Programming/Notes/linkList/linkList.h
@@ -0,0 +1,9 @@
+// header guards
+#ifndef LINKLIST_H
+#define LINKLIST_H
+
+// preprocessor directives
+#include <iostream>
+
+// close header guards
+#endif
---
+++ C_Programming/Structures/lab7structures.c
@@ -0,0 +1,18 @@
+#include "lab7structures.h"
+
+/*
+
+Notes:
+
+// using typedef allows you use use 'Date' in this case instead of struct all the time
+typedef struct
+{
+    // data
+} Date; (Date is the data type)
+
+// in function call, don't include data type, only the variable (data type)
+validate(d1);
+
+unsigned int validate(Date d);
+
+*/
