8b60f90eb2ef131df1caed538dee0d9c7f141400
venkat-ranganathan <prangan1@binghamton.edu>
2022-12-09 19:16:17 -0500

    Ceratonykus

---
+++ C_Programming/Notes/oop/Header1.h
@@ -0,0 +1,93 @@
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class vechicleType
+{
+public:
+	// Constructor
+	// There is never a return type on a constructor
+	// The constructor name is the same as the class name
+	vechicleType() { cout << "Object Created" << endl; }
+
+	// This is a constructor overload
+	// You can have as many overloads as you want
+	vechicleType(string carName) { name = carName; }
+	vechicleType(bool radio);
+
+	// Function inside a class is called method.
+	void SetName(string newCar);
+
+	void PrintHasRadio(int index);
+
+	virtual void foo(void) = 0;
+
+	void PrintHasRadio(void)
+	{
+		cout << "Radio present: " << hasARadio << endl;
+	}
+
+	// Methods written inside a class are called in-line
+	void SetTires(int tires) { numberOfTires = tires; }
+	void SetHasRadio(bool value) { hasARadio = value; }
+
+	// Destructor -- Always starts with a ~
+	// Destructor CANNOT be overloaded and CANNOT have parameters
+	~vechicleType()
+	{
+		static int count = 0;
+		count++;
+
+		cout << "Object number " << count << " has been destroyed" << endl;
+	}
+
+protected:
+	void ClearName(void)
+	{
+		name = "";
+		cout << "Name is clear\n";
+	}
+
+private:
+	string name;
+	int numberOfTires;
+	bool hasARadio;
+};
+
+class car : public vechicleType
+{
+public:
+	car() { SetName("Racer"); }
+	car(string newName) { SetName(newName); }
+
+	void foo() { cout << "Called from car\n"
+					  << endl; }
+
+	~car() { ClearName(); }
+
+private:
+	int value;
+};
+
+class bike : public vechicleType
+{
+public:
+	bike()
+	{
+		SetHasRadio(false);
+		SetTires(2);
+	}
+
+	void foo(void) { cout << "Called from bike\n"; }
+
+private:
+	string peddals;
+	string handleBars;
+};
+
+#endif
---
+++ C_Programming/Notes/oop/Source.cpp
@@ -0,0 +1,47 @@
+
+#include "header.h"
+
+int main(void)
+{
+	vechicleType* pointerArray[2];
+
+	pointerArray[0] = new car();
+	pointerArray[1] = new bike();
+
+	for (int index = 0; index < 2; index++)
+	{
+		pointerArray[index]->foo();
+	}
+
+	for (int index = 0; index < 2; index++)
+	{
+		delete pointerArray[index];
+	}
+	car newCar("Dancer");
+
+	newCar.SetTires(4);
+
+	newCar.foo();
+	
+	bike tike;
+
+	tike.foo();
+
+	return 0;
+}
+
+
+void vechicleType::SetName(string newCar)
+{
+	name = newCar;
+}
+
+vechicleType::vechicleType(bool radio)
+{
+	hasARadio = radio;
+}
+
+void vechicleType::PrintHasRadio(int index)
+{
+	cout << "Radio # " << index << ": " << hasARadio << endl;
+}
