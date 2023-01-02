5bf1a6a8764733d1e98b5531f193d5cdc1aefdc0
venkat-ranganathan <prangan1@binghamton.edu>
2023-01-02 10:35:49 -0500

    Arcusaurus

---
+++ Data_Structures_Algorithms/structFunction/structFunction.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Venkat Ranganathan on 1/1/2023.
+//
+#include "structFunction.h"
+
+int main() { return 0; }
---
+++ Data_Structures_Algorithms/structFunction/structFunction.h
@@ -0,0 +1,10 @@
+//
+// Created by Venkat Ranganathan on 1/1/2023.
+//
+#ifndef PROJECTS_STRUCTFUNCTION_H
+#define PROJECTS_STRUCTFUNCTION_H
+
+#include "cstdio"
+#include "cstdlib"
+
+#endif // PROJECTS_STRUCTFUNCTION_H
